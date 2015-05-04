4514ed3579f5ca2a847526e3df22641c02afd2d3
acordoba_33 <acordoba_33@live.com>
2015-05-04 13:00:55 +0000

    Add the Webservice class

--- scada/scadaService/src/dao/DaoScada.java
+++ scada/scadaService/src/dao/DaoScada.java
@@ -10,82 +10,96 @@ import model.Camara;
 import exceptions.ConnectionErrorException;
 
 public class DaoScada {
-	private java.sql.Connection con = null;
-	private static DaoScada connect;
-	java.sql.ResultSet rs = null;
-	PreparedStatement ps = null;
-	public static DaoScada getInstance() {
-		if (connect == null) {
-			connect = new DaoScada();
-		}
-		return connect;
-	}
 
-	public java.sql.Connection getConnection() throws ClassNotFoundException, SQLException {
-		if (con == null) {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			con = java.sql.DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=SCADA_SAP;user=sap;password=Tecno2015;");
-		} else
-			new ConnectionErrorException();
+    private java.sql.Connection con = null;
+    private static DaoScada connect;
+    java.sql.ResultSet rs = null;
+    PreparedStatement ps = null;
 
-		return con;
-	}
+    public static DaoScada getInstance() {
+        if (connect == null) {
+            connect = new DaoScada();
+        }
+        return connect;
+    }
 
-	
-	public void displayDbProperties() throws ClassNotFoundException, SQLException {
-		java.sql.DatabaseMetaData dm = null;
-			con = this.getConnection();
-			if (con != null) {
-				dm = con.getMetaData();
-				System.out.println("Información del controlador");
-				System.out.println("\tNombre del controlador: " + dm.getDriverName());
-				System.out.println("\tVersion del controlador: " + dm.getDriverVersion());
-				System.out.println("\nInformación de la base de datos ");
-				System.out.println("\tNombre de la base de datos: " + dm.getDatabaseProductName());
-				System.out.println("\tVersiï¿½n de la base de datos: " + dm.getDatabaseProductVersion());
-				System.out.println("Catï¿½logos disponibles ");
-				rs = dm.getCatalogs();
-				while (rs.next()) {
-					System.out.println("\tcatï¿½logo: " + rs.getString(1));
-				}
-				closeConnection();
-			} 
-		dm = null;
-	}
+    public java.sql.Connection getConnection() throws ClassNotFoundException, SQLException {
+        if (con == null) {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = java.sql.DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=SCADA_SAP;user=sap;password=Tecno2015;");
+        } else {
+            new ConnectionErrorException();
+        }
 
-	private void closeConnection() throws SQLException {
-			if(rs != null){
-				rs.close();
-				rs = null;
-			}else new ConnectionErrorException();
-			if (con != null){
-				con.close();
-				con = null;
-			}else new ConnectionErrorException();
-	
-	}
-	
-	public List<Camara> getInformacionDeCamara(String nombreTabla, String fecha) throws ClassNotFoundException, SQLException {
-		List<Camara> registros = new ArrayList<Camara>();
-		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-		con = this.getConnection();
-		String query = "SELECT  VarName,TimeString ,VarValue from " + nombreTabla + " where Cast(TimeString as datetime) >= Cast('"+ fecha + "' as datetime)" ;
-		con = this.getConnection();
-		if (con != null) {
-			ps = null;
-			rs = null;
-			ps = con.prepareStatement(query);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				Camara camara = new Camara();
-				camara.setTimeString(rs.getString("TimeString"));
-				camara.setVarName(rs.getString("VarName"));
-				camara.setVarValue(rs.getString("VarValue"));
-				registros.add(camara);
-			}
-		}
-		this.closeConnection();
-		return registros;
-	}
+        return con;
+    }
+
+    public void displayDbProperties() throws ClassNotFoundException, SQLException {
+        java.sql.DatabaseMetaData dm = null;
+        con = this.getConnection();
+        if (con != null) {
+            dm = con.getMetaData();
+            System.out.println("Informacion del controlador");
+            System.out.println("Nombre del controlador: " + dm.getDriverName());
+            System.out.println("Version del controlador: " + dm.getDriverVersion());
+            System.out.println("\nInformaciï¿½n de la base de datos ");
+            System.out.println("\tNombre de la base de datos: " + dm.getDatabaseProductName());
+            System.out.println("\tVersiï¿½n de la base de datos: " + dm.getDatabaseProductVersion());
+            System.out.println("Catalogos disponibles ");
+            rs = dm.getCatalogs();
+            while (rs.next()) {
+                System.out.println("\tcatï¿½logo: " + rs.getString(1));
+            }
+            closeConnection();
+        }
+        dm = null;
+    }
+
+    private void closeConnection() throws SQLException {
+        if (rs != null) {
+            rs.close();
+            rs = null;
+        } else {
+            new ConnectionErrorException();
+        }
+        if (con != null) {
+            con.close();
+            con = null;
+        } else {
+            new ConnectionErrorException();
+        }
+
+    }
+
+    public List<Camara> getInformacionDeCamara(String nombreTabla, String fecha) throws ClassNotFoundException, SQLException {
+        List<Camara> registros = new ArrayList<Camara>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        con = this.getConnection();
+        String query = "SELECT  VarName,TimeString ,VarValue from " + nombreTabla + " where Cast(TimeString as datetime) >= Cast('" + fecha + "' as datetime)";
+        con = this.getConnection();
+        if (con != null) {
+            ps = null;
+            rs = null;
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Camara camara = new Camara();
+                camara.setTimeString(rs.getString("TimeString"));
+                camara.setVarName(rs.getString("VarName"));
+                camara.setVarValue(rs.getString("VarValue"));
+                registros.add(camara);
+            }
+        } else {
+            Camara camara = new Camara();
+            camara.setTimeString("No hay registro en esa fecha");
+            camara.setVarName("No hay registro en esa fecha");
+            camara.setVarValue("No hay registro en esa fecha");
+            registros.add(camara);
+
+        }
+
+        this.closeConnection();
+        return registros;
+    }
 
 }
